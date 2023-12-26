e6bf962ed6e079b4dd2bbb92dfa7fca1f8e15de7
ErickBackDev <erickson17graterol@gmail.com>
2023-12-26 14:30:44 -0400

    I add the container with the database

---
+++ sql/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customers (first_name, last_name, email, phone) VALUES
+    ('John', 'Doe', 'johndoe@example.com', '555-1234'),
+    ('Jane', 'Smith', 'janesmith@example.com', '555-5678'),
+    ('Michael', 'Johnson', 'michaeljohnson@example.com', '555-9876'),
+    ('Emily', 'Davis', 'emilydavis@example.com', '555-4321');
