a3c1f222585c4dd1092c125ece21cb8b16278b87
ErickBackDev <erickson17graterol@gmail.com>
2023-12-27 16:58:20 -0400

    Update querys

--- faker/faker-service.py
+++ faker/faker-service.py
@@ -13,22 +13,29 @@ logging.debug("FAKER SERVICE STARTED")
 
 # ESTA FUNCION CREARA DATOS ALEATORIOS
 def random_data():
+
     insert_queries = []
     profiles = [dict(itertools.islice(faker.profile().items(),6)) for data in range(13)]
+
     for profile in profiles:
         sql = ""
+
         for key , value in profile.items():
+
             if key == 'current_location':
-                coordinates = [str(coordinate) for coordinate in value]
-                profile.update(current_location = json.dumps({"coordinates":coordinates}))
+                coordinates = [str(coordinate) for coordinate in profile['current_location']]
+                profile[key] = ', '.join(coordinates)
+
         values = (str(list(profile.values())).replace("\\","")[1:-1])
-        sql = f""" INSERT INTO profile (job, company, ssn, residence, current_location, blood_group) VALUES ({values}); """.replace("\n","")
+        sql = f"INSERT INTO profile (job, company, ssn, residence, current_location, blood_group) VALUES ({values});".replace("\n","")
         insert_queries.append(sql)
+
     logging.debug(insert_queries[0])
     return insert_queries
 
 #ESTA FUNCION CREA LA CONEXION A LA DB
-def execute_queries():
+def execute_queries(list_queries_string=[], querie_type=''):
+
     db_name = 'db_psql'
     db_user = 'root'
     db_pass = '12345678'
@@ -40,21 +47,21 @@ def execute_queries():
         db_string = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
         db = create_engine(db_string)
         connection = db.connect()
-        query = text("SELECT version();")
-        data = connection.execute(query)
-        for item in data:
-            logging.debug(item[0])
-        connection.close()
+        if querie_type == 'INSERT':
+            for querie in list_queries_string:
+                query = text(querie)
+                data = connection.execute(query).rowcount
+        return {"message":"success query"}
     except Exception as ex:
         logging.debug(ex)
 
 # SE CREA UN endpoint, Y SI ASINGA UNA URL PARA QUE RESPONDA LA APP
 @app.route('/create_records')
 def create_profiles():
+
     try:
-        data_profiles = random_data()
-        execute_queries()
-        return jsonify(data_profiles)
+        results_querie = execute_queries(random_data(), "INSERT")
+        return jsonify(results_querie)
     except:
         return {"message":"endpoint error"}
     
